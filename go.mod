6fc5f692e5b8fac8d3e9d5ccfba14666e81822c1
Parham Alvani <parham.alvani@gmail.com>
2024-10-21 05:54:51 +0000

    chore: add resolve using go default dns query

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/snapp-incubator/nats-readiness
 go 1.23.2
 
 require (
-	github.com/go-resty/resty/v2 v2.15.3 // indirect
+	github.com/go-resty/resty/v2 v2.15.3
+	go.uber.org/zap v1.27.0
+)
+
+require (
+	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
-import "github.com/go-resty/resty/v2"
+import (
+	"net"
+
+	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
+)
 
 func main() {
+	logger, _ := zap.NewDevelopment()
+
 	client := resty.New()
 
-	client.R().Get("127.0.0.1:8222/healthz")
+	resp, err := client.R().SetQueryParam("js-enabled-only", "1").Get("http://127.0.0.1:8222/healthz")
+	if err != nil {
+		logger.Error("failed to call nats healthz endpoint", zap.Error(err))
+	}
+
+	logger.Info("nats healthz response", zap.ByteString("response", resp.Body()))
+}
+
+// resolve resolves the given headless service into the pods ip address.
+func resolve(headless string) ([]string, error) {
+	ips, err := net.LookupIP(headless)
+	if err != nil {
+		return nil, err
+	}
+
+	var pods []string
+
+	for _, ip := range ips {
+		pods = append(pods, ip.String())
+	}
+
+	return pods, nil
 }
